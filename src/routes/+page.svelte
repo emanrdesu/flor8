3c89da542941515aa21a0a1b0718d06c25790d19
emanrdesu <eperezpozo@knights.ucf.edu>
2023-09-05 17:34:45 -0400

    Flor de Maldad

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { fade, fly } from 'svelte/transition';
+
+	let judge = false;
+	let answer = false;
+
+	function respuesta() {
+		judge = true;
+		answer = Math.random() < 0.5;
+
+		setTimeout(() => (judge = false), 5000);
+	}
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-lg flex flex-col gap-6">
+			<div class="flex items-center">
+				<img class="img w-64" alt="flower" src="/src/lib/image/flor3.png" />
+				<div class="flex flex-col gap-8 items-center">
+					<h1 class="text-5xl font-bold">La flor de maldád</h1>
+					<span class="flex gap-4 items-center">
+						<button on:click={respuesta} class="btn btn-sm w-min btn-info">Dice...</button>
+
+						{#if judge}
+							<div
+								transition:fade={{ delay: 150 }}
+								class="badge badge-lg font-bold"
+								class:badge-success={answer}
+								class:badge-error={!answer}
+							>
+								{#if answer}
+									Si
+								{:else}
+									No
+								{/if}
+							</div>
+						{/if}
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
